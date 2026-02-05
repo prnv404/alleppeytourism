390529b6f0ff5c25d2397a6088db19fb04db7440
prnv404 <connectpranavs@gmail.com>
2026-02-05 20:46:15 +0530

    updates

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://alleppeytourism.com';
+    const baseUrl = 'https://alleppeytourism.in';
 
     return {
         rules: [
